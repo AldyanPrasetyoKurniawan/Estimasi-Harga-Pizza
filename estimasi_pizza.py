d718e2df5aa00ce7c63e282770e30959e4e2e7cf
AldyanPrasetyoKurniawan <132220795+AldyanPrasetyoKurniawan@users.noreply.github.com>
2023-05-01 20:10:20 +0700

    Update estimasi_pizza.py

--- estimasi_pizza.py
+++ estimasi_pizza.py
@@ -7,11 +7,13 @@ st.title('Estimasi Harga Pizza (Rp)')
 
 size = st.number_input('Small, Reguler, Medium, Large, XL, Jumbo (1, 2, 3, 4, 5, 6)')
 diameter = st.number_input('Diameter Pizza dalam Inch')
+extra_sauce = st.number_input('Tambahan Saus (yes = 1 dan no = 0')
+extra_cheesee = st.number_input('Tambahan Keju (yes = 1 dan no = 0')
 
 predict = ''
 
 if st.button('Estimasi Harga'):
     predict = model.predict(
-        [[size, diameter]]
+        [[size, diameter, extra_sauce, extra_cheesee]]
     )
     st.write ('Estimasi harga Pizza : ', predict)
